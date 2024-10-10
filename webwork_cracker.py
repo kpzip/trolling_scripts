1a1a41b97e51c2317e28c11382b3de2ef59a80f6
kpzip <kpzipgames@gmail.com>
2024-10-10 14:38:53 -0700

    add file

---
+++ webwork_cracker.py
@@ -0,0 +1,15 @@
+token = ""
+course_id = "22786"
+assignment_id = "281951"
+
+
+
+
+headers = {
+    "Origin": "https://ucsb.instructure.com",
+    "Referer": f"https://ucsb.instructure.com/courses/{course_id}/assignments/{assignment_id}"
+}
+
+body_tags = {
+    "": ""
+}
