c94ef43f284f46611b3705e4759afa217dc262a4
kpzip <kpzipgames@gmail.com>
2024-11-13 18:44:26 -0800

    config

---
+++ ucsb_diamond/rust impl/ucsb_diamond/src/config.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    username: String,
+    password: String,
+    classes: Vec<ClassInfo>
+}
+
+impl Config {
+    pub fn username(&self) -> &str {
+        &self.username
+    }
+
+    pub fn password(&self) -> &str {
+        &self.password
+    }
+
+    pub fn classes(&self) -> &Vec<ClassInfo> {
+        &self.classes
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ClassInfo {
+    name: String,
+    preferred_code: String,
+    alt_codes: Vec<String>,
+}
+
+impl ClassInfo {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn preferred_code(&self) -> &str {
+        &self.preferred_code
+    }
+
+    pub fn alt_codes(&self) -> &Vec<String> {
+        &self.alt_codes
+    }
+}
