bc8cd78ff58aaabf198c7567e2519756d1a462fe
kpzip <kpzipgames@gmail.com>
2024-11-13 18:44:32 -0800

    Update main.rs

--- ucsb_diamond/rust impl/ucsb_diamond/src/main.rs
+++ ucsb_diamond/rust impl/ucsb_diamond/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use reqwest::blocking::Client;
+use crate::config::Config;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    println!("UCSB Diamond v{}", env!("CARGO_PKG_VERSION"));
+    println!("Reading Config...");
+    let cfg: Config = serde_json::from_str(fs::read_to_string("config.json").unwrap().as_str()).unwrap();
+    println!("Connecting to UCSB Gold...");
+    let client = Client::new();
+    let login_page = client.get("https://sso.ucsb.edu/cas/login?service=https%3a%2f%2fmy.sa.ucsb.edu%2fgold%2fAlertMessage.aspx");
+
+
 }
